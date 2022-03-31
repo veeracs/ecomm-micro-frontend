a61916b8582567e4a09a1e9d616db4d0f8d88c53
Chandra Sekhar Veera <veeracs@gmail.com>
2022-03-31 10:28:25 -0400

    add cart micro frontend

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -1,17 +1,17 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
     mode: 'development',
     devServer: {
-        port: 8080
+        port: 8082
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                products: 'products@http://localhost:8081/remoteEntry.js',
-                cart: 'cart@http://localhost:8082/remoteEntry.js',
+            name: 'cart',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './ShowCart': './src/index'
             }
         }),
         new HtmlWebpackPlugin({
--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,3 +1,4 @@
 import 'products/ProductsIndex';
+import 'cart/ShowCart';
 
 console.log('Container!!!');
